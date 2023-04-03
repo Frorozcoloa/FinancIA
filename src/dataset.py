0373705a461e46672a4b06c2cc16849f708ac673
frorozcol <freorozcoloa@gmail.com>
2023-04-03 16:05:03 -0500

    Addes the multi-label model

--- src/dataset.py
+++ src/dataset.py
@@ -35,51 +35,70 @@ class FinanciaSentimental(Dataset):
         )
         return inputs['input_ids'].squeeze(0), inputs['attention_mask'].squeeze(0), label
     
-
-class BERTClassifier(pl.LightningModule):
+class FinanciaMultilabel(pl.LightningModule):
     """This class is used to create the model"""
-    def __init__(self, labels, model_name):
+    def __init__(self, num_labels=3, model_name='roberta-base'):
         super().__init__()
-        self.labels = labels
-        self.bert = BertForSequenceClassification.from_pretrained(model_name, num_labels=len(labels)))
-        
+        self.num_labels = num_labels
+        #TODO: change the model by model in spanish.
+        self.roberta = RobertaModel.from_pretrained(model_name)
+        self.classifier = nn.Sequential(
+            nn.Linear(self.roberta.config.hidden_size, 128),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(128, self.num_labels),
+            nn.Sigmoid()
+        )
+        self.loss = nn.BCELoss()
+
     def forward(self, input_ids, attention_mask):
         """This function is used to forward the data through the model"""
-        return self.bert(input_ids, attention_mask=attention_mask)[0]
-        
+        output = self.roberta(input_ids, attention_mask=attention_mask)
+        output = output[0]
+        output = self.classifier(output)
+        return output
+
     def training_step(self, batch, batch_idx):
         """This function is used to train the model"""
         input_ids, attention_mask, labels = batch
         outputs = self(input_ids, attention_mask)
-        predictions = torch.argmax(outputs, dim=1)
         train_loss = self.loss(outputs, labels)
-        train_acc = accuracy(predictions, labels)
-        train_f1 = f1(predictions, labels, num_classes=self.num_labels, average='macro')
+        train_acc = accuracy(outputs, labels)
+        train_f1 = f1(outputs, labels)
         wandb.log({'train_loss': train_loss, 'train_acc': train_acc, 'train_f1': train_f1})
-        return {'loss': train_loss}
-        
+        return {'loss': train_loss, 'train_acc': train_acc, 'train_f1': train_f1}
+
     def validation_step(self, batch, batch_idx):
         """This function is used to validate the model"""
         input_ids, attention_mask, labels = batch
         outputs = self(input_ids, attention_mask)
-        predictions = torch.argmax(outputs, dim=1)
         val_loss = self.loss(outputs, labels)
-        val_acc = accuracy(predictions, labels)
-        val_f1 = f1(predictions, labels, num_classes=self.num_labels, average='macro')
+        val_acc = accuracy(outputs, labels)
+        val_f1 = f1(outputs, labels)
         wandb.log({'val_loss': val_loss, 'val_acc': val_acc, 'val_f1': val_f1})
-        return {'val_loss': val_loss}
+        return {'val_loss': val_loss, 'val_acc': val_acc, 'val_f1': val_f1}
+
+    def test_step(self, batch, batch_idx):
+        """This function is used to test the model"""
+        input_ids, attention_mask, labels = batch
+        outputs = self(input_ids, attention_mask)
+        test_loss = self.loss(outputs, labels)
+        test_acc = accuracy(outputs, labels)
+        test_f1 = f1(outputs, labels)
+        wandb.log({'test_loss': test_loss, 'test_acc': test_acc, 'test_f1': test_f1})
+        return {'test_loss': test_loss, 'test_acc': test_acc, 'test_f1': test_f1}
 
     def test_epoch_end(self, outputs):
-        """This function is used to test the model"""
-        test_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-        test_acc = torch.stack([x['test_acc'] for x in outputs]).mean()
-        test_f1 = torch.stack([x['test_f1'] for x in outputs]).mean()
-        wandb.log({'test_loss': test_loss, 'test_acc': test_acc, 'test_f1': test_f1})
-        
+        """This function is used to aggregate test results"""
+        avg_test_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
+        avg_test_acc = torch.stack([x['test_acc'] for x in outputs]).mean()
+        avg_test_f1 = torch.stack([x['test_f1'] for x in outputs]).mean()
+        wandb.log({'avg_test_loss': avg_test_loss, 'avg_test_acc': avg_test_acc, 'avg_test_f1': avg_test_f1})
+        return {'avg_test_loss': avg_test_loss, 'avg_test_acc': avg_test_acc, 'avg_test_f1': avg_test_f1}
+
     def configure_optimizers(self):
         """This function is used to configure the optimizer"""
-        optimizer = torch.optim.AdamW(self.parameters(), lr=2e-5)
-        return optimizer
+        optimizer = torch.optim.AdamW
 
 """
 model = BERTClassifier('bert-base-uncased', num_classes=2)
